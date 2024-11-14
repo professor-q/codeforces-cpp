55c9943d03aec62257427846d8d3d7636b9049fb
professor-q <username.ki.jinis@gmail.com>
2024-11-14 04:56:05 +0000

    A. 1 to N

---
+++ Assiut-University-Training-Newcomers/Sheet-2/A-1-to-N.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        cout << i << '\n';
+    }
+    
+    return 0;
+}
