654a6fde3a760347d7784557253d2f262874525e
professor-q <username.ki.jinis@gmail.com>
2024-11-14 16:41:47 +0000

    K. Divisors

---
+++ Assiut-University-Training-Newcomers/Sheet-2/K-Divisors.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    int n;
+    cin >> n;
+    vector<int> divisors;
+    
+    for (int i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            divisors.push_back(i);
+            if (i * i != n) {
+                divisors.push_back(n / i);
+            }
+        }
+    }
+
+    sort(divisors.begin(), divisors.end());
+
+    for (auto x: divisors) {
+        cout << x << '\n';
+    }
+
+    return 0;
+}
