f7b9f7796ee186f14b3e7219f5e5668963258ea2
professor-q <username.ki.jinis@gmail.com>
2024-11-17 10:15:23 +0000

    G. Conversion

--- Assiut-University-Training-Newcomers/Sheet-4/G-Conversion.cpp
+++ Assiut-University-Training-Newcomers/Sheet-4/G-Conversion.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
  
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
