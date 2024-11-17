922f175291f3dd55d1f1ce8a424868049b670701
professor-q <username.ki.jinis@gmail.com>
2024-11-17 10:37:32 +0000

    D. Positions in array

---
+++ Assiut-University-Training-Newcomers/Sheet-3/D-Positions-in-array.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    int n, arr[1000];
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] < 11) {
+            cout << "A[" << i <<"] = " << arr[i] << '\n';
+        }
+    }
+
+    return 0;
+}
